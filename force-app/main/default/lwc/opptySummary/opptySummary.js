17cc01204dedd132968832d39d085caec9408f42
tnascimento <tnascimento@salesforce.com>
2022-05-31 16:56:46 -0300

    making the component looks better and fixing a bug

--- force-app/main/default/lwc/opptySummary/opptySummary.js
+++ force-app/main/default/lwc/opptySummary/opptySummary.js
@@ -1,6 +1,7 @@
 import { LightningElement, api, wire } from 'lwc';
 import OPPORTUNITY_OBJECT from "@salesforce/schema/Opportunity";
 import { getObjectInfo } from "lightning/uiObjectInfoApi";
+import getOpportunitiesByRecordType from "@salesforce/apex/OpportunityService.getOpportunitiesByRecordType";
 
 export default class OpptySummary extends LightningElement {
     @api recordId;
@@ -21,6 +22,19 @@ export default class OpptySummary extends LightningElement {
             // Returns a map of record type Ids 
             const rtis = this.objectInfo.data.recordTypeInfos;
             return Object.keys(rtis).find(rti => rtis[rti].name === 'Service');        
+        }
+    }
+
+    @wire(getOpportunitiesByRecordType, {accountId: "$recordId", rtId: "$productRTId"})
+    totalOpptyProduct;
+
+    @wire(getOpportunitiesByRecordType, {accountId: "$recordId", rtId: "$serviceRTId"})
+    totalOpptyService;
+
+    get totalOpportunityProducts() {
+        if (this.totalOpptyProduct) {
+            const totalTemp = this.totalOpptyProduct;
+            return totalTemp;
         }
 
     }
